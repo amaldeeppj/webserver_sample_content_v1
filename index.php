12c1e9f77f78f48cd8e5837d885d796edfaef689
Amaldeep P J <amaldeeppjcoc1@gmail.com>
2022-12-13 19:47:32 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Created by Amaldeep -->
+<title>
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {background-color:#030303;background-repeat:no-repeat;background-position:top left;background-attachment:fixed;}
+h1{text-align:center;font-family:Times, serif;color:#28bf1d;background-color:#030303;}
+p {text-align:center;font-family:Cursive;font-size:20px;font-style:normal;font-weight:normal;color:#28bf1d;background-color:#030303;}
+a:link {
+  color: #28bf1d;
+  background-color: transparent;
+  text-decoration: none;
+}
+
+a:visited {
+  color: red;
+  background-color: transparent;
+  text-decoration: none;
+}
+
+a:hover {
+  color: red;
+  background-color: transparent;
+  text-decoration: underline;
+}
+
+a:active {
+  color: blue;
+  background-color: transparent;
+  text-decoration: underline;
+}
+</style>
+</head>
+<body>
+<h1>||</h1>
+<h1>||</h1>
+<h1>X</h1>
+<h1  style="text-decoration: underline;"><?php echo "Contents are loading from: ".gethostname(); ?></h1>
+<p>This is a sample code to identify the running instance.</p>
+<p>Sample code by Amaldeep</p>
+<p><a href="https://github.com/amaldeeppj">https://github.com/amaldeeppj</a></p>
+<p></p>
+<p></p>
+</body>
+</html>
